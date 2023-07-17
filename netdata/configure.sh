7ca8f2bb2fa3f28535a95a5aeaba6cc88e0def1b
Vincent Boon <vincent@justbetter.nl>
2023-07-17 15:03:25 +0200

    Fix slack config seds

--- netdata/configure.sh
+++ netdata/configure.sh
@@ -60,7 +60,7 @@ cp /usr/lib/netdata/conf.d/health_alarm_notify.conf "$config_dir"
 if [ -n "$slack_url" ]; then
     echo "Setting Slack webhook"
     sed -i "s|SLACK_WEBHOOK_URL=.*|SLACK_WEBHOOK_URL=\"$slack_url\"|" "$config_dir/health_alarm_notify.conf"
-    sed -i "s|SEND_SLACK=.*|SEND_SLACK=\"YES\"|" "health_alarm_notify.conf"
+    sed -i "s|SEND_SLACK=.*|SEND_SLACK=\"YES\"|" "$config_dir/health_alarm_notify.conf"
     sed -i "s|DEFAULT_RECIPIENT_SLACK=.*|DEFAULT_RECIPIENT_SLACK=\"#netdata\"|" "$config_dir/health_alarm_notify.conf"
 fi
 
