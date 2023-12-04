df7a28f080d19b9a60bd132956011b1587b2b88f
Vincent Boon <vincent@justbetter.nl>
2023-12-04 14:49:48 +0100

    Fix typo

--- netdata/configure.sh
+++ netdata/configure.sh
@@ -111,7 +111,7 @@ do
             echo "    socket: '/tmp/fpm-status-$path'" >> $netdataFpmConfigFile
         done
 
-        serice php-$version-fpm restart
+        service php-$version-fpm restart
     fi
 done
 
